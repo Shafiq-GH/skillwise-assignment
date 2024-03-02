e7002ccad33462e33d4d105b45f05e1db230a1b7
Shafiq-GH <shafiqsulaimannn@gmail.com>
2024-03-02 08:32:50 +0800

    first

---
+++ scripts/loadScripts.js
@@ -0,0 +1,10 @@
+function loadScript(src) {
+  var script = document.createElement("script");
+  script.src = src;
+  document.head.appendChild(script);
+}
+
+loadScript("https://kit.fontawesome.com/a076d05399.js");
+loadScript("https://code.jquery.com/jquery-3.5.1.slim.min.js");
+loadScript("https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.6/dist/umd/popper.min.js");
+loadScript("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js");
